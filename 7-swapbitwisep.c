bdaca5a9f5549dbb1a94e3da0c64022fca84053d
MinaFarid1 <mina.awed10@yahoo.com>
2022-11-26 15:36:13 +0200

    modify 7th solution

--- 7-swapbitwisep.c
+++ 7-swapbitwisep.c
@@ -4,12 +4,13 @@ int main()
 {
     int x, y;
     int *p1 = &x, *p2 = &y;
+    void (*swap_ptr)(int *, int *) = &swap;
     printf("Enter first number: "); 
     scanf("%d", p1);
     printf("Enter second number: ");
     scanf("%d", p2);
     printf("\nFirst number before swapping: %d \nSecond number before swapping: %d \n\n", x, y);
-    swap(p1, p2);
+    (*swap_ptr)(p1, p2);
     printf("First number after swapping: %d \nSecond number after swapping: %d", x, y);
 }
 void swap(int *x, int *y){
