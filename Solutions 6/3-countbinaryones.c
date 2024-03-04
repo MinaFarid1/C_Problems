4291a3a3d82750d378375bdbf02e916f979de237
MinaFarid1 <mina.awed10@yahoo.com>
2024-03-04 05:02:45 +0200

    Added new problems

---
+++ Solutions 6/3-countbinaryones.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// الاسم: مينا فريد سمير - سكشن: 7
+int countOnes(int num) {
+    int count = 0;
+    while (num != 0) { // Loop untill no more 1s exist
+        count += num & 1; // Add the result of bitwise AND 1 with the least significant bit
+        num >>= 1; // Right shift the number to check the next bit
+    }
+    return count;
+}
+
+int main() {
+    int number;
+    printf("Enter an integer number: ");
+    scanf("%d", &number);
+    printf("Number of ones in the binary representation: %d\n", countOnes(number));
+
+    return 0;
+}
---
+++ Solutions 6/4-reverse8bitbinary.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// الاسم: مينا فريد سمير - سكشن: 7
+unsigned char reverseBinary(unsigned char num) {
+    unsigned char reversed = 0;
+
+    for (int i = 0; i < 8; i++) {
+        reversed <<= 1;             // Left shift the result to make room for the next bit
+        reversed |= num & 1;        // OR operation with the least significant bit of num
+        num >>= 1;                  // Right shift num to process the next bit
+    }
+
+    return reversed;
+}
+
+int main() {
+    unsigned char number = 0b10001101;  // 0x8D
+    unsigned char reversed = reverseBinary(number); // Result should be 1011 0001 (0xB1)
+    
+    printf("Number: 0x%02X\n", number);
+    printf("Reversed: 0x%02X\n", reversed);
+
+    return 0;
+}
